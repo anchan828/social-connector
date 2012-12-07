581981c6a2b291bb71bdbd0a5e15ec82508ccaaa
kyusyukeigo <anchan828@gmail.com>
2012-12-08 03:41:31 +0900

    Editor Default Resourcesフォルダを削除した。~Keyクラスを外部に出した refs #1

--- Assets/SocialConnector/Editor/SocialConnectorPreferences.cs
+++ Assets/SocialConnector/Editor/SocialConnectorPreferences.cs
@@ -16,7 +16,7 @@ public class SocialConnectorPreferences
 	}
 	
 	
-	
+	///==========================///
 	
 	
 	
@@ -53,6 +53,43 @@ public class SocialConnectorPreferences
 		rect.y += 25;
 		GUI.TextField (rect, "<color=#00ffffff><b>https://github.com/anchan828/social-connector</b></color>", WindowSettings.style);
 	}
+	
+	
+	///=====================================================///
+	///
+	///	 public
+	///
+	///=====================================================///
+	
+	
+	/// <summary>
+	/// Gets the name of the project.
+	/// </summary>
+	/// <value>
+	/// The name of the project.
+	/// </value>
+	public static string projectName {
+		get {
+			string[] split = Application.dataPath.Split ('/');
+			return split [split.Length - 2];
+		}
+	}
+	/// <summary>
+	/// Gets the editor folder path.
+	/// </summary>
+	/// <value>
+	/// The editor folder path.
+	/// </value>
+	/// <example>
+	/// Assets/SocialConnector/Editor/
+	/// </example>
+	public static string editorFolderPath {
+		get {
+			string currentFilePath = new System.Diagnostics.StackTrace (true).GetFrame (0).GetFileName ();
+			return "Assets" + currentFilePath.Substring (0, currentFilePath.LastIndexOf ('/') + 1).Replace (Application.dataPath, string.Empty);
+		}
+	}
+	
 	
 	
 	///=====================================================///
@@ -108,137 +145,6 @@ public class SocialConnectorPreferences
 		}
 		
 		private static GUIStyle _style = null;
-	}
-	
-	/// <summary>
-	/// Twitter  Infomation.
-	/// </summary>
-	private static class TwitterKey
-	{
-		public const string serviceName = "Twitter";
-		public const string keyName = "Consumer Key";
-		public const string secretName = "Consumer Secret Key";
-		private static string _consumerKey = string.Empty;
-		private static string _consumerSecret = string.Empty;
-		private static Texture2D _logo = null;
-		
-		public static string consumerKey {
-			get {
-				
-				if (string.IsNullOrEmpty (_consumerKey))
-					_consumerKey = EditorPrefs.GetString (projectName + "_twitter_consumerKey");
-				
-				return _consumerKey;
-			}
-			set {
-				
-				if (value.Equals (_consumerKey))
-					return;
-				
-				EditorPrefs.SetString (projectName + "_twitter_consumerKey", value);
-				_consumerKey = value;
-			}
-		}
-		
-		public static string consumerSecret {
-			get {
-				
-				if (string.IsNullOrEmpty (_consumerSecret))
-					_consumerSecret = EditorPrefs.GetString (projectName + "_twitter_consumerSecret");
-				
-				return _consumerSecret;
-			}
-			set {
-				if (value.Equals (_consumerSecret))
-					return;
-				
-				EditorPrefs.SetString (projectName + "_twitter_consumerSecret", value);
-				_consumerSecret = value;
-			}
-		}
-	
-		public static Texture2D logo {
-			get {
-				if (_logo != null)
-					return _logo;
-				
-				_logo = (Texture2D)EditorGUIUtility.LoadRequired ("twitter.png");
-				return _logo;
-			}
-		}
-	}
-	
-	/// <summary>
-	/// Facebook Infomation.
-	/// </summary>
-	private static class FacebookKey
-	{
-		public const string serviceName = "Facebook";
-		public const string keyName = "API Key";
-		public const string secretName = "API Secret Key";
-		private static string _apiKey = string.Empty;
-		private static string _apiSecret = string.Empty;
-		private static Texture2D _logo = null;
-
-		public static string apiKey {
-			get {
-				
-				if (string.IsNullOrEmpty (_apiKey))
-					_apiKey = EditorPrefs.GetString (projectName + "_facebook_apiKey");
-				
-				
-				return _apiKey;
-			}
-			set {
-				
-				if (value.Equals (_apiKey))
-					return;
-				EditorPrefs.SetString (projectName + "_facebook_apiKey", value);
-				_apiKey = value;
-			}
-		}
-		
-		public static string apiSecret {
-			get {
-				
-				if (string.IsNullOrEmpty (_apiSecret))
-					_apiSecret = EditorPrefs.GetString (projectName + "_facebook_apiSecret");
-				
-				return _apiSecret;
-			}
-			set {
-				
-				if (value.Equals (_apiSecret))
-					return;
-				
-				EditorPrefs.SetString (projectName + "_facebook_apiSecret", value);
-				_apiSecret = value;
-			}
-		}
-
-		public static Texture2D logo {
-			get {
-				if (_logo != null)
-					return _logo;
-				
-				_logo = (Texture2D)EditorGUIUtility.LoadRequired ("facebook.png");
-				return _logo;
-			}
-		}
-	
-	}
-	
-	/// <summary>
-	/// Gets the name of the project.
-	/// </summary>
-	/// <value>
-	/// The name of the project.
-	/// </value>
-	private static string projectName {
-		get {
-			string[] split = Application.dataPath.Split ('/');
-			return split [split.Length - 2];
-		}
 	}
 	
 }

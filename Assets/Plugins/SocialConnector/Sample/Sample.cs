672e02e1ecabb92cb530262f63ff8006a22517dc
Keigo Ando <anchan828@gmail.com>
2013-12-26 23:22:51 +0900

    画像添付できるようにした

--- Assets/Plugins/SocialConnector/Sample/Sample.cs
+++ Assets/Plugins/SocialConnector/Sample/Sample.cs
@@ -2,33 +2,53 @@ using UnityEngine;
 
 public class Sample : MonoBehaviour
 {
+	string imagePath {
+		get {
+			return  Application.persistentDataPath + "/image.png";
+		}
+	}
+
+	void Start ()
+	{
+	}
+
 	void OnGUI ()
 	{
+		GUILayout.Label (imagePath);
+
+		if (GUILayout.Button ("Take")) {
+			Application.CaptureScreenshot ("image.png");
+		}
+
 		///=================
 		/// Twitter
 		///=================
-	
-		if (GUILayout.Button ("<size=50><b>Twitter</b></size>")) {
+
+		if (GUILayout.Button ("Twitter")) {
 			SocialConector.PostMessage (SocialConector.ServiceType.Twitter);
 		}
-		if (GUILayout.Button ("<size=50><b>Twitter text</b></size>")) {
+		if (GUILayout.Button ("Twitter<size=50><b> text</b></size>")) {
 			SocialConector.PostMessage (SocialConector.ServiceType.Twitter, "text");
 		}
-		if (GUILayout.Button ("<size=50><b>Twitter text&url</b></size>")) {
+		if (GUILayout.Button ("Twitter<size=50><b> text&url</b></size>")) {
 			SocialConector.PostMessage (SocialConector.ServiceType.Twitter, "text", "http://japan.unity3d.com/");
+		}
+
+		if (GUILayout.Button ("Twitter<size=50><b> text&url&image</b></size>")) {
+			SocialConector.PostMessage (SocialConector.ServiceType.Twitter, "text", "http://japan.unity3d.com/", imagePath);
 		}
 		
 		///=================
 		/// Facebook
 		///=================
 		
-		if (GUILayout.Button ("<size=50><b>Facebook</b></size>")) {
+		if (GUILayout.Button ("Facebook")) {
 			SocialConector.PostMessage (SocialConector.ServiceType.Facebook);
 		}
-		if (GUILayout.Button ("<size=50><b>Facebook text</b></size>")) {
+		if (GUILayout.Button ("Facebook<size=50><b> text</b></size>")) {
 			SocialConector.PostMessage (SocialConector.ServiceType.Facebook, "text");
 		}
-		if (GUILayout.Button ("<size=50><b>Facebook text&url</b></size>")) {
+		if (GUILayout.Button ("Facebook<size=50><b> text&url</b></size>")) {
 			SocialConector.PostMessage (SocialConector.ServiceType.Facebook, "text", "http://japan.unity3d.com/");
 		}
 		
@@ -36,13 +56,13 @@ public class Sample : MonoBehaviour
 		/// LINE
 		///=================
 		
-		if (GUILayout.Button ("<size=50><b>LINE</b></size>")) {
+		if (GUILayout.Button ("LINE")) {
 			SocialConector.PostMessage (SocialConector.ServiceType.Line);
 		}
-		if (GUILayout.Button ("<size=50><b>LINE text</b></size>")) {
+		if (GUILayout.Button ("LINE<size=50><b> text</b></size>")) {
 			SocialConector.PostMessage (SocialConector.ServiceType.Line, "text");
 		}
-		if (GUILayout.Button ("<size=50><b>LINE text&url</b></size>")) {
+		if (GUILayout.Button ("LINE<size=50><b> text&url</b></size>")) {
 			SocialConector.PostMessage (SocialConector.ServiceType.Line, "text", "http://japan.unity3d.com/");
 		}
 	}
--- Assets/Plugins/SocialConnector/SocialConector.cs
+++ Assets/Plugins/SocialConnector/SocialConector.cs
@@ -5,12 +5,10 @@ using UnityEngine;
 using System.Runtime.InteropServices;
 
 #endif
-
 public class SocialConector
 {
+	#if UNITY_IPHONE
 	
-#if UNITY_IPHONE
-
 	[DllImport("__Internal")]
 	private static extern bool IsAvailableForServiceType_( int type );
 	
@@ -18,15 +16,11 @@ public class SocialConector
 	private static extern void PostMessage_( int type, string text, string url ); 
 
 #elif UNITY_ANDROID
-	
-	private static AndroidJavaObject clazz = new AndroidJavaClass ( "com.unity3d.player.UnityPlayer" );
-	private static AndroidJavaObject activity = clazz.GetStatic<AndroidJavaObject> ( "currentActivity" );
+	private static AndroidJavaObject clazz = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
+	private static AndroidJavaObject activity = clazz.GetStatic<AndroidJavaObject> ("currentActivity");
 
-#endif
-	
-	
-	
-#if UNITY_IPHONE
+	#endif
+	#if UNITY_IPHONE
 	
 	private static void _PostMessage ( ServiceType type, string text, string url )
 	{
@@ -41,56 +35,68 @@ public class SocialConector
 		} 
 	}
 
-	
 #elif UNITY_ANDROID
-	
-	private static void _PostMessage (ServiceType type, string text, string url)
+	private static void _PostMessage (ServiceType type, string text, string url, string textureUrl)
 	{
 		string packageName = string.Empty;
-		
-		if(type.Equals(ServiceType.Twitter)){
+
+		if (type.Equals (ServiceType.Twitter)) {
 			packageName = "com.twitter.android";
-		}else if (type.Equals(ServiceType.Facebook)){
+		} else if (type.Equals (ServiceType.Facebook)) {
 			packageName = "com.facebook.katana";
-		}else if(type.Equals(ServiceType.Line)){
-			Application.OpenURL(string.Format("line://msg/text/{0}", System.Uri.EscapeUriString( text + (string.IsNullOrEmpty( url ) ? "" : " - " + url) ) ) );
+		} else if (type.Equals (ServiceType.Line)) {
+			Application.OpenURL (string.Format ("line://msg/text/{0}", System.Uri.EscapeUriString (text + (string.IsNullOrEmpty (url) ? "" : " - " + url))));
 			return;
 		}
-		
-		AndroidJavaObject intent = new AndroidJavaObject ( "android.content.Intent" );
-	
-		
-		intent.Call<AndroidJavaObject> ( "setPackage", packageName );
-		intent.Call<AndroidJavaObject> ( "setType", "text/plain" );
-		intent.Call<AndroidJavaObject> ( "putExtra", "android.intent.extra.TEXT" , text + "\t" + url );
-		activity.Call ("startActivity", intent.CallStatic<AndroidJavaObject>( "createChooser", intent, "Sorry" ));
-		intent.Dispose ();
+
+		using (AndroidJavaObject intent = new AndroidJavaObject ("android.content.Intent")) {
+
+			intent.Call<AndroidJavaObject> ("setAction", "android.intent.action.SEND");
+			intent.Call<AndroidJavaObject> ("setPackage", packageName);
+			intent.Call<AndroidJavaObject> ("setType", "image/png");
+
+			if (!string.IsNullOrEmpty (url))
+				text += "\t" + url;
+			if (!string.IsNullOrEmpty (text))
+				intent.Call<AndroidJavaObject> ("putExtra", "android.intent.extra.TEXT", text);
+
+			if (!string.IsNullOrEmpty (textureUrl)) {
+				AndroidJavaClass uri = new AndroidJavaClass ("android.net.Uri");
+				AndroidJavaObject file = new AndroidJavaObject ("java.io.File", textureUrl);
+				intent.Call<AndroidJavaObject> ("putExtra", "android.intent.extra.STREAM", uri.CallStatic<AndroidJavaObject> ("fromFile", file));
+			}
+			AndroidJavaObject chooser = intent.CallStatic<AndroidJavaObject> ("createChooser", intent, "");
+			// TODO 複数対応
+			chooser.Call<AndroidJavaObject> ("putExtra", "android.intent.extra.EXTRA_INITIAL_INTENTS", intent);
+			activity.Call ("startActivity", chooser);
+		}
 	}
 	
-#endif
-	
-	
-	
+	#endif
 	public enum ServiceType
 	{
 		Twitter = 0,
 		Facebook = 1,
-		Line =2
+		Line = 2
 	}
-	
-	
+
 	public static void PostMessage (ServiceType type)
 	{
-		_PostMessage (type, null, null);
+		_PostMessage (type, null, null, null);
 	}
-	
+
 	public static void PostMessage (ServiceType type, string text)
 	{
-		_PostMessage (type, text, null);
+		_PostMessage (type, text, null, null);
 	}
 
 	public static void PostMessage (ServiceType type, string text, string url)
 	{
-		_PostMessage (type, text, url);
+		_PostMessage (type, text, url, null);
+	}
+
+	public static void PostMessage (ServiceType type, string text, string url, string textureUrl)
+	{
+		_PostMessage (type, text, url, textureUrl);
 	}
 }

e51412fcf0e10a0f879a78e38c7183575fb23591
Keigo Ando <anchan828@gmail.com>
2014-05-26 18:40:34 +0900

    API - PostMessageの削除 Mac対応削除 ignore追記

--- Assets/Plugins/iOS/SocialConnector.mm
+++ Assets/Plugins/iOS/SocialConnector.mm
@@ -5,14 +5,9 @@
 //  Created by Ando Keigo on 2012/12/08.
 //
 //
-#import <QuartzCore/QuartzCore.h>
+#if UNITY_VERSION <= 434
 #import "iPhone_View.h"
-#import "Social/Social.h"
-
-#define TWITTER 0
-#define FACEBOOK 1
-#define LINE 2
-
+#endif
 extern "C" {
 
 @interface SocialActivity : UIActivity
@@ -63,7 +58,7 @@ void *(^_myblock)(void);
     if ([self isInstalled])
         return [NSClassFromString(@"UIActivity") _activityImageForApplication:_bundleIdentifier];
     else {
-        return [NSClassFromString(@"UIActivity") _activityFunctionImage:[UIImage imageNamed:@"SampleAcitvity.png"]];
+        return nil;
     }
 }
 
@@ -146,71 +141,4 @@ void SocialConnector_Share(const char *text, const char *url, const char *textur
     [UnityGetGLViewController() presentViewController:activityView animated:YES completion:nil];
 }
 
-void SocialConnector_PostMessage(const int *type, const char *text, const char *url, const char *textureURL) {
-
-    NSString *_text = [NSString stringWithUTF8String:text ? text : ""];
-    NSString *_url = [NSString stringWithUTF8String:url ? url : ""];
-    NSString *_textureURL = [NSString stringWithUTF8String:textureURL ? textureURL : ""];
-
-
-    if (type == (int const *) LINE) {
-        NSString *contentType;
-        NSString *contentKey;
-
-        if ([_textureURL length] != 0) {
-            UIImage *image = [UIImage imageWithContentsOfFile:_textureURL];
-            if (image == nil) {
-                return;
-            }
-
-            UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
-            pasteboard.image = image;
-            contentType = @"image";
-            contentKey = pasteboard.name;
-
-        } else if ([_text length] != 0) {
-            contentType = @"text";
-            contentKey = _text;
-
-            if ([_url length] != 0) {
-                contentKey = [contentKey stringByAppendingFormat:@"%%20%%2d%%20%@", _url];
-            }
-        }
-
-        NSURL *lineUrl = [NSURL URLWithString:[NSString stringWithFormat:@"line://msg/%@/%@", contentType, contentKey]];
-
-        [[UIApplication sharedApplication] openURL:lineUrl];
-
-    } else {
-
-        SLComposeViewController *composeViewController = [SLComposeViewController composeViewControllerForServiceType:type == TWITTER ? SLServiceTypeTwitter : SLServiceTypeFacebook];
-
-        composeViewController.completionHandler = ^(SLComposeViewControllerResult res) {
-            if (res == SLComposeViewControllerResultCancelled) {
-                // Cancel
-                // UnitySendMessage(<#(char const *)obj#>, <#(char const *)method#>, <#(char const *)msg#>)
-            }
-            else if (res == SLComposeViewControllerResultDone) {
-                // done!
-                // UnitySendMessage(<#(char const *)obj#>, <#(char const *)method#>, <#(char const *)msg#>)
-            }
-            [composeViewController dismissViewControllerAnimated:YES completion:nil];
-        };
-
-        if ([_text length] != 0) {
-            [composeViewController setInitialText:_text];
-        }
-        if ([_url length] != 0) {
-            [composeViewController addURL:[NSURL URLWithString:_url]];
-        }
-        if ([_textureURL length] != 0) {
-            UIImage *image = [UIImage imageWithContentsOfFile:_textureURL];
-            if (image != nil) {
-                [composeViewController addImage:image];
-            }
-        }
-
-        [UnityGetGLViewController() presentViewController:composeViewController animated:YES completion:nil];
-    }
-}
 }

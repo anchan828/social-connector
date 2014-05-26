801e70df1d6d24c44e65bd88cd56f29336d747a9
Keigo Ando <anchan828@gmail.com>
2014-05-26 17:18:58 +0900

    Share機能追加

--- Assets/Plugins/SocialConnector/SocialConnector.cs
+++ Assets/Plugins/SocialConnector/SocialConnector.cs
@@ -7,107 +7,156 @@ using System.Runtime.InteropServices;
 #endif
 public class SocialConnector
 {
-	#if UNITY_IPHONE
-	[DllImport ("__Internal")]
-	private static extern void SocialConnector_PostMessage (int type, string text, string url, string textureUrl);
+#if UNITY_IPHONE
+    [DllImport("__Internal")]
+    private static extern void SocialConnector_PostMessage(int type, string text, string url, string textureUrl);
 
-	#elif UNITY_ANDROID
-	private static AndroidJavaObject clazz = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
-	private static AndroidJavaObject activity = clazz.GetStatic<AndroidJavaObject> ("currentActivity");
+    [DllImport("__Internal")]
+    private static extern void SocialConnector_Share(string text, string url, string textureUrl);
+
+#elif UNITY_ANDROID
+    private static AndroidJavaObject clazz = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+    private static AndroidJavaObject activity = clazz.GetStatic<AndroidJavaObject>("currentActivity");
 
 
-	#elif UNITY_STANDALONE_OSX
+#elif UNITY_STANDALONE_OSX
 	[DllImport ("SocialConnector")]
 	private static extern void SocialConnector_PostMessage (int type, string text, string url, string textureUrl);
 
-	#endif
+#endif
 
-	#if UNITY_IPHONE || UNITY_STANDALONE_OSX
-	
-	private static void _PostMessage (ServiceType type, string text, string url, string textureUrl)
-	{
-			SocialConnector_PostMessage ((int)type, text, url, textureUrl);
-	}
+#if UNITY_IPHONE || UNITY_STANDALONE_OSX
 
-	#elif UNITY_ANDROID
-	private static void _PostMessage (ServiceType type, string text, string url, string textureUrl)
-	{
-		string packageName = string.Empty;
+    private static void _Share(string text, string url, string textureUrl)
+    {
+        SocialConnector_Share(text, url, textureUrl);
+    }
 
-		if (type.Equals (ServiceType.Twitter)) {
-			packageName = "com.twitter.android";
-		} else if (type.Equals (ServiceType.Facebook)) {
-			packageName = "com.facebook.katana";
-		} else if (type.Equals (ServiceType.Line)) {
+    private static void _PostMessage(ServiceType type, string text, string url, string textureUrl)
+    {
+        SocialConnector_PostMessage((int)type, text, url, textureUrl);
+    }
 
-			string contentType = "";
-			string contentKey = "";
+#elif UNITY_ANDROID
 
-			if(string.IsNullOrEmpty(textureUrl)){
-				contentType = "text";
-				contentKey = text;
+    private static void _Share(string text, string url, string textureUrl)
+    {
+        ActionSend(text, url, textureUrl, null);
+    }
 
-				if(!string.IsNullOrEmpty (url)){
-					contentKey += " - " + url;
-				}
-			}else{
-				contentType = "image";
-				contentKey = textureUrl;
-			}
+    private static void _PostMessage(ServiceType type, string text, string url, string textureUrl)
+    {
+        var packageName = string.Empty;
 
-			string lineUrl = string.Format ("line://msg/{0}/{1}", contentType, contentKey);
-			Application.OpenURL (lineUrl);
-			return;
-		}
+        if (type.Equals(ServiceType.Twitter))
+        {
+            packageName = "com.twitter.android";
+        }
+        else if (type.Equals(ServiceType.Facebook))
+        {
+            packageName = "com.facebook.katana";
+        }
+        else if (type.Equals(ServiceType.Line))
+        {
+            string contentType;
+            string contentKey;
 
-		using (AndroidJavaObject intent = new AndroidJavaObject ("android.content.Intent")) {
+            if (string.IsNullOrEmpty(textureUrl))
+            {
+                contentType = "text";
+                contentKey = text;
 
-			intent.Call<AndroidJavaObject> ("setAction", "android.intent.action.SEND");
-			intent.Call<AndroidJavaObject> ("setPackage", packageName);
-			intent.Call<AndroidJavaObject> ("setType", "image/png");
+                if (!string.IsNullOrEmpty(url))
+                {
+                    contentKey += " - " + url;
+                }
+            }
+            else
+            {
+                contentType = "image";
+                contentKey = textureUrl;
+            }
 
-			if (!string.IsNullOrEmpty (url))
-				text += "\t" + url;
-			if (!string.IsNullOrEmpty (text))
-				intent.Call<AndroidJavaObject> ("putExtra", "android.intent.extra.TEXT", text);
+            var lineUrl = string.Format("line://msg/{0}/{1}", contentType, contentKey);
+            Application.OpenURL(lineUrl);
+            return;
+        }
 
-			if (!string.IsNullOrEmpty (textureUrl)) {
-				AndroidJavaClass uri = new AndroidJavaClass ("android.net.Uri");
-				AndroidJavaObject file = new AndroidJavaObject ("java.io.File", textureUrl);
-				intent.Call<AndroidJavaObject> ("putExtra", "android.intent.extra.STREAM", uri.CallStatic<AndroidJavaObject> ("fromFile", file));
-			}
-			AndroidJavaObject chooser = intent.CallStatic<AndroidJavaObject> ("createChooser", intent, "");
-			// TODO 複数対応
-			chooser.Call<AndroidJavaObject> ("putExtra", "android.intent.extra.EXTRA_INITIAL_INTENTS", intent);
-			activity.Call ("startActivity", chooser);
-		}
-	}
-	
-	#endif
-	public enum ServiceType
-	{
-		Twitter = 0,
-		Facebook = 1,
-		Line = 2
-	}
+        ActionSend(text, url, textureUrl, packageName);
+    }
 
-	public static void PostMessage (ServiceType type)
-	{
-		_PostMessage (type, null, null, null);
-	}
+    private static void ActionSend(string text, string url, string textureUrl, string packageName)
+    {
+        using (var intent = new AndroidJavaObject("android.content.Intent"))
+        {
+            intent.Call<AndroidJavaObject>("setAction", "android.intent.action.SEND");
+            intent.Call<AndroidJavaObject>("setType", string.IsNullOrEmpty(textureUrl) ? "text/plain" : "image/png");
 
-	public static void PostMessage (ServiceType type, string text)
-	{
-		_PostMessage (type, text, null, null);
-	}
+            if (!string.IsNullOrEmpty(packageName))
+            {
+                intent.Call<AndroidJavaObject>("setPackage", packageName);
+            }
 
-	public static void PostMessage (ServiceType type, string text, string url)
-	{
-		_PostMessage (type, text, url, null);
-	}
+            if (!string.IsNullOrEmpty(url))
+                text += "\t" + url;
+            if (!string.IsNullOrEmpty(text))
+                intent.Call<AndroidJavaObject>("putExtra", "android.intent.extra.TEXT", text);
 
-	public static void PostMessage (ServiceType type, string text, string url, string textureUrl)
-	{
-		_PostMessage (type, text, url, textureUrl);
-	}
+            if (!string.IsNullOrEmpty(textureUrl))
+            {
+                var uri = new AndroidJavaClass("android.net.Uri");
+                var file = new AndroidJavaObject("java.io.File", textureUrl);
+                intent.Call<AndroidJavaObject>("putExtra", "android.intent.extra.STREAM", uri.CallStatic<AndroidJavaObject>("fromFile", file));
+            }
+            var chooser = intent.CallStatic<AndroidJavaObject>("createChooser", intent, "");
+            chooser.Call<AndroidJavaObject>("putExtra", "android.intent.extra.EXTRA_INITIAL_INTENTS", intent);
+            activity.Call("startActivity", chooser);
+        }
+    }
+#endif
+
+
+    public static void Share(string text)
+    {
+        Share(text, null, null);
+    }
+
+    public static void Share(string text, string url)
+    {
+        Share(text, url, null);
+    }
+
+    public static void Share(string text, string url, string textureUrl)
+    {
+        _Share(text, url, textureUrl);
+    }
+
+
+
+    public enum ServiceType
+    {
+        Twitter = 0,
+        Facebook = 1,
+        Line = 2
+    }
+
+    public static void PostMessage(ServiceType type)
+    {
+        _PostMessage(type, null, null, null);
+    }
+
+    public static void PostMessage(ServiceType type, string text)
+    {
+        _PostMessage(type, text, null, null);
+    }
+
+    public static void PostMessage(ServiceType type, string text, string url)
+    {
+        _PostMessage(type, text, url, null);
+    }
+
+    public static void PostMessage(ServiceType type, string text, string url, string textureUrl)
+    {
+        _PostMessage(type, text, url, textureUrl);
+    }
 }
